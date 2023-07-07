9dbe2655f8615c83b548a973f4bef7384494deb1
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-07 11:37:35 +0400

    stage0Module3Task4

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+        int i = 9;
+        int j = 5;
+        int y = 2;
+        int l = 7;
+        int h = 4;
+
+        System.out.println(((i + a) - (j + b) + y + a - l) * (a + b - h + a + b) / y);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        int k = c;
+        int j = h;
+        int l = a;
+        int g = r;
+
+        System.out.println(k);
+        System.out.println(j);
+        System.out.println(l);
+        System.out.println(g);
     }
+
 }
